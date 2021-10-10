4559bf3b57fd6709fc0dd8fa9deef0d87fb1215f
MatthewSzy <matthewszymone@gmail.com>
2021-10-10 15:47:59 +0200

    first commit

---
+++ src/main/java/com/example/MovieRank/MovieRankApplication.java
@@ -0,0 +1,13 @@
+package com.example.MovieRank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MovieRank/MovieRankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MovieRank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
